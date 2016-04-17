f3c96990669030710ea477a0cd22d815e7a20ce2
edupooch <edupooch@gmail.com>
2016-04-17 12:22:26 -0300

    commit

--- app/src/main/java/br/edu/ufcspa/tc6m/controle/CronometroActivity.java
+++ app/src/main/java/br/edu/ufcspa/tc6m/controle/CronometroActivity.java
@@ -3,44 +3,53 @@ package br.edu.ufcspa.tc6m.controle;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Chronometer;
-import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.DateFormat;
 
 import br.edu.ufcspa.tc6m.R;
 
 public class CronometroActivity extends AppCompatActivity {
 
-    private FloatingActionButton btStart;
+    private FloatingActionButton btAdicionarVolta;
     private Chronometer crono;
     private TextView textDistancia;
-    ////////
+    private LinearLayout layoutFrase;
+    private TextView textFrase;
+    private TextView textDadosMinuto;
+    ////////////////////////////////
     private long miliseconds;
     private int metros;
     private int volta;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cronometro);
+        iniciaComponentes();
+
+
+    }
+
+    private void iniciaComponentes() {
+        ////////////////////////////////////////////////////////////////////////////////////////////
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setVisibility(View.GONE);
         setSupportActionBar(toolbar);
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        textFrase = (TextView) findViewById(R.id.textApoio);
+        textDadosMinuto = (TextView) findViewById(R.id.textDadosMinuto);
+        ////////////////////////////////////////////////////////////////////////////////////////////
         miliseconds = 0;
         metros = 0;
         volta = 20;
-
-        textDistancia = (TextView)findViewById(R.id.textMetros);
         crono = (Chronometer) findViewById(R.id.cronometro);
-
         crono.setBase(SystemClock.elapsedRealtime() - miliseconds);
         crono.start();
         crono.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
@@ -48,27 +57,95 @@ public class CronometroActivity extends AppCompatActivity {
             public void onChronometerTick(Chronometer chronometer) {
                 miliseconds = SystemClock.elapsedRealtime() - crono.getBase();
 
-                if (miliseconds >= 360000) {
-
-                    chronometer.stop();
-                    crono.stop();
-                    Toast.makeText(getApplication(), "Teste Concluído", Toast.LENGTH_LONG).show();
+                if (chronometer.getText().toString().equals("01:00")) {
+                    umMinuto();
                 }
+                if (chronometer.getText().toString().equals("02:00")) {
+                    doisMinutos();
+                }
+                if (chronometer.getText().toString().equals("03:00")) {
+                    tresMinutos();
+                }
+                if (chronometer.getText().toString().equals("04:00")) {
+                    quatroMinutos();
+                }
+                if (chronometer.getText().toString().equals("05:00")) {
+                    cincoMinutos();
+                }
+
+                if (chronometer.getText().toString().equals("06:00")) {
+                    chronometer.stop();
+                    seisMinutos();
+                }
+
+
             }
         });
 
-        btStart = (FloatingActionButton) findViewById(R.id.btStart);
-        btStart.setOnClickListener(new View.OnClickListener() {
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        textDistancia = (TextView) findViewById(R.id.textMetros);
+
+        btAdicionarVolta = (FloatingActionButton) findViewById(R.id.btAdicionarVolta);
+        btAdicionarVolta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                metros+=volta;
-                textDistancia.setText(metros);
-
+                Toast.makeText(getApplication(), "" + Integer.parseInt(crono.getText().toString().replace(":", "")), Toast.LENGTH_LONG).show();
+                metros += volta;
+                textDistancia.setText(String.valueOf(metros));
             }
         });
+        ////////////////////////////////////////////////////////////////////////////////////////////
 
+        layoutFrase = (LinearLayout) findViewById(R.id.layoutFrase);
 
+        layoutFrase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layoutFrase.setVisibility(View.GONE);
+            }
+        });
     }
 
+
+    //////////////////////////////////////////MINUTOS///////////////////////////////////////////////
+    private void umMinuto() {
+        textFrase.setText(R.string.frase1);
+        textDadosMinuto.setText(R.string.dados1);
+        layoutFrase.setVisibility(View.VISIBLE);
+    }
+
+
+    private void doisMinutos() {
+        textFrase.setText(R.string.frase2);
+        textDadosMinuto.setText(R.string.dados2);
+
+        layoutFrase.setVisibility(View.VISIBLE);
+    }
+
+    private void tresMinutos() {
+        textFrase.setText(R.string.frase3);
+        textDadosMinuto.setText(R.string.dados3);
+        layoutFrase.setVisibility(View.VISIBLE);
+    }
+
+
+    private void quatroMinutos() {
+        textFrase.setText(R.string.frase4);
+        textDadosMinuto.setText(R.string.dados4);
+        layoutFrase.setVisibility(View.VISIBLE);
+    }
+
+
+    private void cincoMinutos() {
+        textFrase.setText(R.string.frase5);
+        textDadosMinuto.setText(R.string.dados5);
+        layoutFrase.setVisibility(View.VISIBLE);
+    }
+
+
+    private void seisMinutos() {
+        textDadosMinuto.setText(R.string.dados6);
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////
 }
