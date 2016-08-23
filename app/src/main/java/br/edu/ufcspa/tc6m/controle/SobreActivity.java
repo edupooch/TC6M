b75a88a9fde83480eebc8fbb81237541c3626da2
edupooch <edupooch@gmail.com>
2016-08-23 12:02:19 -0300

    - PErfil atualizado

--- app/src/main/java/br/edu/ufcspa/tc6m/controle/SobreActivity.java
+++ app/src/main/java/br/edu/ufcspa/tc6m/controle/SobreActivity.java
@@ -11,7 +11,7 @@ public class SobreActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_perfil2);
+        setContentView(R.layout.activity_sobre);
 
     }
 }
